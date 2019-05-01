dfcbb0c518a010ac9730b097736c44c3b104c248
saurabh9495 <saurabhkumar4829@gmail.com>
2019-05-01 15:27:46 +0530

    Take input via map and list

--- Basics/basic2.py
+++ Basics/basic2.py
@@ -1,4 +1,4 @@
-x = list(map(int,input("Enter multiple valuses: ").split()))
+x = list(map(int, input("Enter multiple valuses: ").split()))
 print(x)
-x,y=[int(x) for x in input("Enter two value").split()]
-print(x,y)
+x, y = [int(x) for x in input("Enter two value").split()]
+print(x, y)
