5fc302bf6a39e6bf58f570faf8c4b09a20e96524
saurabh9495 <saurabhkumar4829@gmail.com>
2019-05-01 14:49:06 +0530

    Basic Calc

--- Basics/basic.py
+++ Basics/basic.py
@@ -2,29 +2,20 @@
 # Python program for simple calculator
 
 # Function to add two numbers
-
-
 def add(num1, num2):
     return num1 + num2
 
 # Function to subtract two numbers
-
-
 def subtract(num1, num2):
     return num1 - num2
 
 # Function to multiply two numbers
-
-
 def multiply(num1, num2):
     return num1 * num2
 
 # Function to divide two numbers
-
-
 def divide(num1, num2):
     return num1 / num2
-
 
 print("Please select operation -\n"
       "1. Add\n"
